f34088facdccff4390676e955c603eb8dc3cbf82
Junho Choi <tig9659@ajou.ac.kr>
2023-11-05 11:21:19 +0900

    build: 배포 환경 수정

--- src/app.ts
+++ src/app.ts
@@ -11,18 +11,22 @@ app.use(express.urlencoded( {extended : false } ));
 app.use(bodyParser.json());
 app.use(router)
 
-app.get('/', async(req: Request,res: Response)=>{
-    fs.readFile('./src/public/main.html', (err: NodeJS.ErrnoException | null, data: Buffer)=>{
-        if(err) throw err;
+if (process.env.NODE_ENV !== 'production') {
+    app.get('/', async (req: Request, res: Response) => {
+        fs.readFile('./src/public/main.html', (err: NodeJS.ErrnoException | null, data: Buffer) => {
+            if (err) throw err;
 
-        res.writeHead(200,{
-            'Content-Type' : 'text/html'
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.write(data)
+            res.end();
         })
-        res.write(data)
-        res.end();
     })
-})
+}
+
 
 app.listen(port,()=>{
-    console.log('server running on http://localhost:'+port);
+    if (process.env.NODE_ENV !== 'production')
+        console.log('server running on http://localhost:'+port);
 })
